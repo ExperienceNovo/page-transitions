831866693aea67413c6080c1b6d7a081d2b16250
evolvedus <evolvedus@gmail.com>
2015-09-01 20:14:35 -0400

    all inner html unedited

--- js/inner-html.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#page1").load("page1.html");
-    $("#page2").load("page2.html");
-    $("#page3").load("page3.html");
-    $("#page4").load("page4.html");
-});
-
-
